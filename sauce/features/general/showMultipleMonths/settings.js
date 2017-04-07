4ea214ee943a3098cf5a6cc40e0eda92c5658f58
Alan Hoskins <alanhoskins@gmail.com>
2017-04-06 23:41:15 -0400

    Initial Commit

---
+++ sauce/features/general/showMultipleMonths/settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'ShowMultipleMonths',
+  type: 'checkbox',
+  default: false,
+  section: 'general',
+  title: 'Multiple Month Budget View',
+  description: 'Adds button to the sidebar to access a multiple month budget view.'
+};
